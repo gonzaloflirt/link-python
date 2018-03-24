ab95d85b4923eaa0d62c8144c2ab4c3abde6be99
gonzalo <gonzalo.flirt@gmail.com>
2018-03-24 21:21:47 +0100

    Minor cleanup

--- example/LinkHut.py
+++ example/LinkHut.py
@@ -13,21 +13,24 @@ l = link.Link(120)
 l.enabled = True
 l.startStopSyncEnabled = True
 
-while True:
-  s = l.captureSessionState()
-  link_time = l.clock().micros();
-  tempo_str = "{0:.2f}".format(s.tempo())
-  beats_str = "{0:.2f}".format(s.beatAtTime(link_time, 0))
-  playing_str = str(s.isPlaying())
-  phase = s.phaseAtTime(link_time, 4)
-  phase_str = ''
-  for x in range(0, 4):
-    if x < phase:
-      phase_str += 'X'
-    else:
-      phase_str += "0"
-  sys.stdout.write(
-      'tempo ' + tempo_str + ' | isPlaying: ' + playing_str + ' | beats ' + beats_str
+try:
+  while True:
+    s = l.captureSessionState()
+    link_time = l.clock().micros();
+    tempo_str = '{0:.2f}'.format(s.tempo())
+    beats_str = '{0:.2f}'.format(s.beatAtTime(link_time, 0))
+    playing_str = str(s.isPlaying())
+    phase = s.phaseAtTime(link_time, 4)
+    phase_str = ''
+    for x in range(0, 4):
+      if x < phase:
+        phase_str += 'X'
+      else:
+        phase_str += '0'
+    sys.stdout.write(
+      'tempo ' + tempo_str + ' | playing: ' + playing_str + ' | beats ' + beats_str
       + ' | ' + phase_str + '  \r')
-  sys.stdout.flush()
-  time.sleep(0.02)
+    sys.stdout.flush()
+    time.sleep(0.02)
+except KeyboardInterrupt:
+    pass
